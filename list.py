688d73adf264dc6dfa2ea68a981d580cb32ce311
eric090 <eperegrina@ifae.es>
2018-12-17 14:25:20 +0100

    Add files via upload

--- list.py
+++ list.py
@@ -1,59 +1,53 @@
 import os
 import pickle
 
-PICKLE_FILE = "names.pkl"
-list_names = ['Otger B.', 'Juan B.', 'Laia C.', 'Jorge G.', 'Alex G.', 'Pepe I.', 'Jorge J.', 'Oscar M.', 'Julià M.', 'Eric P.', 'Cristobal P.', 'David R.', 'Marti V.']
-names = {'Names': list_names}
 
-list_assist = [len(list_names)]
-list_clean = [len(list_names)]
 
-F = open('names.pkl','wb')
-pickle.dump(names,F)
-F.close()
-
-def newUser():
-    os.system('cls')
-    new_name = input("Insert name: ")
-    list_names.append(new_name)
-    names = {'Names': list_names}
+#list_names = ['Otger B.', 'Juan B.', 'Laia C.', 'Jorge G.', 'Alex G.', 'Pepe I.', 'Jorge J.', 'Oscar M.', 'Julià M.', 'Eric P.', 'Cristobal P.', 'David R.', 'Marti V.']
+#names = {'Names': list_names}
+data = []
+def newfile():
     F = open('names.pkl', 'wb')
-    pickle.dump(names, F)
     F.close()
-    menu()
-def deleteUser():
+    print("File created")
+
+def newUser(name):
     os.system('cls')
-    deletename = input("Insert name: ")
+    a = {'name' : name}
+    data.append(a)
+    print(len(data))
+    #list_names.append(a)
+    F = open('names.pkl', 'wb')
+    pickle.dump(data,F)
+
+    F.close()
+def deleteUser(name):
+    os.system('cls')
+    delete_name = {'name': name}
     error = True
-    for x in range (0, len(list_names)):
-        if deletename == list_names[x]:
-            list_names.pop(x)
-            names = {'Names': list_names}
+
+    for x in range(0, len(data)):
+        if delete_name == data[x]:
+            data.pop("name", name)
             F = open('names.pkl', 'wb')
-            pickle.dump(names, F)
+            pickle.dump(data, F)
             F.close()
-            print("Name deleted succes")
+            print("Name delet1ed succes")
             error = False
     if error:
         print("Name incorrect, please enter the name again")
-        deleteUser()
+        deleteUser(delete_name)
 
-def showUser():
+def showList():
     os.system('cls')
-
     e = open('names.pkl','rb')
     print(pickle.load(e))
-    #for item in read_from_pickle(PICKLE_FILE):
 
-    #os.remove(PICKLE_FILE)
 
-def add_to_pickle(path, item):
-    with open(path, 'ab') as file:
-        pickle.dump(item, file, pickle.HIGHEST_PROTOCOL)
-def read_from_pickle(path):
-    with open(path, 'rb') as file:
-        try:
-            while True:
-                yield pickle.load(file)
-        except EOFError:
-            pass
+def add_assist(name):
+    F = open('names.pkl', 'wb')
+    list_names = F
+    for i in list_names:
+        if name == list_names[i]:
+            list_names[i]["assist: "] +=1
+
--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 import list
+import os.path
 
-def menu():
-    os.system('cls')
+if os.path.isfile('names.pkl'):
+    print("File already exist")
+else:
+    list.newfile()
+
+def main():
     print ("Select option:\n")
     print("1 - New user\n")
     print("2 - Delete user\n")
     print("3 - Show users\n")
     print("4 - Add Assists\n")
     print("9 - Exit")
-
-
 while True:
-    menu()
+    main()
     optionMenu = input("Insert value >> ")
     if optionMenu == "1":
         print("")
-        input("Has pulsado la opción 1...\n")
-        list.newUser()
+        print("Has pulsado la opción 1...\n")
+        name = input("Insert name: ")
+        list.newUser(name)
     elif optionMenu == "2":
         print("")
-        input("Has pulsado la opción 2...\n")
-        list.deleteUser()
+        print("Has pulsado la opción 2...\n")
+        name = input("Insert name: ")
+        list.deleteUser(name)
     elif optionMenu == "3":
         print("")
-        input("Has pulsado la opción 3...\n")
-        list.showUser()
+        print("Has pulsado la opción 3...\n")
+        list.showList()
     elif optionMenu == "4":
         print("")
-        input("Has pulsado la opción 4...\n")
+        print("Has pulsado la opción 4...\n")
+        name = input("Insert name: ")
+        list.add_assist(name)
        # asssist()
     elif optionMenu == "9":
         break
